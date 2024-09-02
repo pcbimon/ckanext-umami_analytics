570f41e8a68845d17d12efa25ee4dc3cb1351c36
pcbimon <patipat.che@mahidol.edu>
2024-09-02 15:20:54 +0700

    feat: add integrate service js + add track event view-dataset

--- ckanext/umami_analytics/plugin.py
+++ ckanext/umami_analytics/plugin.py
@@ -1,5 +1,7 @@
+from ckan.common import CKANConfig
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import os
 
 
 class UmamiAnalyticsPlugin(plugins.SingletonPlugin):
@@ -8,9 +10,15 @@ class UmamiAnalyticsPlugin(plugins.SingletonPlugin):
 
     # IConfigurer
 
-    def update_config(self, config_):
+    def update_config(self, config_: 'CKANConfig'):
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "umami_analytics")
+        umami_instance = os.getenv('UMAMI_INSTANCE', '')
+        site_id = os.getenv('UMAMI_SITE_ID', '')
+        
+        # Inject the script into the <head> section
+        script_tag = f'<script defer src="{umami_instance}/script.js" data-website-id="{site_id}"></script>'
+        config_['ckan.template_head_end'] = config_.get('ckan.template_head_end', '') + script_tag
 
     
